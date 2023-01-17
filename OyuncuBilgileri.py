b6c8265631179b9dc65aeb0796070af5aff928e6
Yusuf Kızılarslan <122723162+Yusufkizilarslan0@users.noreply.github.com>
2023-01-17 21:07:47 +0300

    Add files via upload

---
+++ OyuncuBilgileri.py
@@ -0,0 +1,19 @@
+print("Oyuncu Kaydetme Programı")
+
+ad = input("Oyuncunun Adı:")
+soyad =  input("Oyuncunun Soyadı:")
+takım = input("Oyuncunun Takımı:")
+
+Bilgiler = [ad,soyad,takım]
+
+print("Bilgiler Kaydediliyor ....")
+
+
+print("Oyuncu Adı: {}\nOyuncu Soyadı: {}\n Oyuncunun Takımı: {}\n".format(Bilgiler[0],Bilgiler[1],Bilgiler[2]))
+
+
+
+print("Bilgiler Kaydedildi....")
+
+
+
---
+++ İkinciDerecedenDenklemHesaplama.py
@@ -0,0 +1,25 @@
+### 
+2. Dereceden bir bilinmeyenli denklemin köklerini bulma
+
+Denklem : ax^2 + bx + c
+
+Deltayı hesaplama: b**2 - 4 * a * c
+
+Birinci kök = (-b + delta ** 0.5) / (2*a)
+İkinci Kök =  (-b + delta ** 0.5) / (2*a)
+
+
+###
+
+a = int(input("a:"))
+b = int(input("b:"))
+c = int(input("c:"))
+
+
+delta = b ** 2 - 4 * a * c 
+
+
+x1 = (-b - delta ** 0.5) / ( 2 * a )
+x2 = (-b + delta ** 0.5) / ( 2 * a )
+
+print("Birinci Kök: {}\nİkinci kök: {}\n".format(x1,x2))
